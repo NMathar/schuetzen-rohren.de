1c02409eeaa91398c84b956adb6d6e184c9230fb
nm254744 <nicolai.mathar@ima.rwth-aachen.de>
2025-12-18 08:14:33 +0100

    add deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   ssr: false,
+  nitro: {
+    preset: 'static'
+  },
   
   modules: ['vuetify-nuxt-module', '@nuxtjs/google-fonts', 'nuxt-swiper', 'nuxt-graphql-client'],
   
